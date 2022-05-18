dff1d92557237c1d947a9484af764f43132a4aa9
unknown <vimalkuriensam@gmail.com>
2022-05-18 23:32:46 +0530

    greet fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn greet() -> impl Responder {
+    HttpResponse::Ok().body("Hello World from Vimal")
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
 }
