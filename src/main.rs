997fb046923411993fef94a11d3fc5b4a1d85a2b
unknown <vimalkuriensam@gmail.com>
2022-05-20 17:47:13 +0530

    health check api

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use std::io::Result;
 
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
-async fn greet() -> impl Responder {
-    HttpResponse::Ok().body("Hello World from Vimal")
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    HttpServer::new(|| App::new().service(greet))
+    HttpServer::new(|| App::new().service(health_check))
         .bind(("127.0.0.1", 8000))?
         .run()
         .await
